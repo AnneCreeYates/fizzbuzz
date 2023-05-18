82aabdd1df8dc31af26b4bbbb036413e5f6bc1cc
Anna Edmund <annecreeyates@gmail.com>
2023-05-18 01:16:08 +0100

    in index.html: added className to the section where numbers are generated; in main.js updated the fizzbuzz function so that it adds text to HTML and also changes colour of 'fizz', 'buzz' and 'fizzbuzz' words to make them stand out a bit more

--- main.js
+++ main.js
@@ -2,16 +2,39 @@ const start_btn = document.getElementById("start_btn");
 
 const fizzbuzz_func = () => {
   for (let i = 0; i < 100; i++) {
+    let span = document.createElement("span");
     if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0) {
-      console.log("FizzBuzz");
+      // this part needs to add visualisation for the words in the animation section
+      //   document.getElementsByClassName("generatedText")[0].textContent +=
+      //     " FizzBuzz,";
+      span.textContent = " FizzBuzz, ";
+      span.style.color = "purple";
     } else if ((i + 1) % 3 == 0) {
-      console.log("Fizz");
+      // this part needs to add visualisation for the words in the animation section
+      //   document.getElementsByClassName("generatedText")[0].textContent +=
+      //     " Fizz,";
+      span.textContent = " Fizz, ";
+      span.style.color = "blue";
     } else if ((i + 1) % 5 == 0) {
-      console.log("Buzz");
+      // this part needs to add visualisation for the words in the animation section
+      //   document.getElementsByClassName("generatedText")[0].textContent +=
+      //     " Buzz,";
+      span.textContent = " Buzz, ";
+      span.style.color = "red";
     } else {
-      console.log(i + 1);
+      // this part needs to add visualisation for the number in the animation section
+      //   document.getElementsByClassName("generatedText")[0].textContent +=
+      //     " " + (i + 1) + ",";
+      span.textContent = " " + (i + 1) + ",";
+      span.style.color = "black";
     }
+    document.getElementsByClassName("generatedText")[0].appendChild(span);
   }
 };
+
+// make a function removing the start button
+// make a function adding the pause, stop and reset buttons
+
+// add function for functionality of every button
 
 start_btn.addEventListener("click", fizzbuzz_func);
